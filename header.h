4c7f9b05081e9b637a348645b6a43e618869c292
RahmadHidayatullah <166033080+Ryonn12@users.noreply.github.com>
2025-04-26 08:07:17 +0700

    update files

--- header.h
+++ header.h
@@ -13,3 +13,22 @@ void generate_random_words(const char *filename, int count, int max_word_length)
 void clearScreen();
 void confirmButton();
 void clearInputBuffer();
+void printTableHeader(void);
+void printTableRow(const char* algorithm, int count, double time_taken, size_t memory_used);
+void printTableFooter(void);
+
+// Fungsi Sorting
+void bubbleSort(const char *filename, int count, int dataType);
+void selectionSort(const char *filename, int count, int dataType);
+void insertionSort(const char *filename, int count, int dataType);
+void mergeSort(const char *filename, int count, int dataType);
+void quickSort(const char *filename, int count, int dataType);
+void shellSort(const char *filename, int count, int dataType);
+void quickSort(const char *filename, int count, int dataType);
+int partitionInt(int arr[], int low, int high);
+void quickSortInt(int arr[], int low, int high);
+void mergeInt(int arr[], int l, int m, int r);
+void mergeSortInt(int arr[], int l, int r);
+void mergeStr(char **arr, int l, int m, int r);
+void mergeSortStr(char **arr, int l, int r);
+void mergeSort(const char *filename, int count, int dataType);
--- main.c
+++ main.c
@@ -17,13 +17,33 @@ int main(void)
     enum DataLength lengthOption;
     enum SortMethod methodOption;
 
+    // cek data, jika tidak ada maka generate ulang
+    FILE *fp = fopen("dataNumber.txt", "r");
+    if (!fp) {
+        puts("< File dataNumber.txt tidak ditemukan, membuat data baru... >");
+        Sleep(1200);
+        generate_random_numbers("dataNumber.txt", 200000000, 2000000); // contoh: 20 juta angka acak, max 2000000
+    } else {
+        fclose(fp); // file ditemukan, tutup kembali
+    }
+
+    fp = fopen("dataWord.txt", "r");
+    if (!fp) {
+        puts("< File dataWord.txt tidak ditemukan, membuat data baru... >");
+        Sleep(1200);
+        generate_random_words("dataWord.txt", 20000000, 20); // contoh: 20 juta kata acak, max panjang 20
+    } else {
+        fclose(fp);
+    }
+    const char *fileName[]   = { "", "dataNumber.txt", "dataWord.txt"};
+
     while(1){
+        DataType:
         clearScreen();
-        DataKindReturn:
         /* ── menu 1: jenis data ─────────────────────────────── */
         puts("Masukkan jenis data yang akan di-sorting :");
-        puts("(1) Data Acak Berupa Angka sebanyak 2.000.000");
-        puts("(2) Data Acak Berupa Huruf sebanyak 2.000.000");
+        puts("(1) Data Acak Berupa Angka sebanyak 2.000.000 angka");
+        puts("(2) Data Acak Berupa Huruf sebanyak 2.000.000 kata");
         puts("(3) Keluar dari program");
         printf("Masukkan Pilihan : ");
         scanf("%d", (int*)&dataOption);
@@ -32,8 +52,8 @@ int main(void)
             break;
         }
 
+        DataLength:
         clearScreen();
-        DataLengthReturn:
         /* ── menu 2: ukuran data ────────────────────────────── */
         puts("\nPilih Jumlah data yang ingin diuji :");
         puts("(1) 10.000");
@@ -48,92 +68,139 @@ int main(void)
         printf("Masukkan Pilihan : ");
         scanf("%d", (int*)&lengthOption);
         clearInputBuffer();  // Bersihkan buffer setelah input
-        if(lengthOption == 3){
-            goto DataLengthReturn;
+        if(lengthOption == 9){
+            goto DataType;
         }
 
-        clearScreen();
         /* ── menu 3: metode sorting ─────────────────────────── */
-        SortMethodReturn:
-        puts("\nPilih metode Sorting :");
+        clearScreen();
+        puts("\nPilih Metode Sorting :");
         puts("(1) Bubble Sort");
         puts("(2) Selection Sort");
         puts("(3) Insertion Sort");
         puts("(4) Merge Sort");
         puts("(5) Quick Sort");
         puts("(6) Shell Sort");
-        puts("(7) Kembali");
+        puts("(7) Semua Metode");
+        puts("(8) Kembali");
         printf("Masukkan Pilihan : ");
         scanf("%d", (int*)&methodOption);
         clearInputBuffer();  // Bersihkan buffer setelah input
-        if(methodOption == 3){
-            goto SortMethodReturn;
+        if(methodOption == 8){
+            goto DataLength;
         }
 
         /* ── cetak rekap pilihan ───────────────────────────── */
-        const char *namaData[]   = { "", "Angka", "Huruf" };
-        const char *namaLength[] = { "",
-            "10.000", "50.000", "100.000", "250.000",
-            "500.000", "1.000.000", "1.500.000", "2.000.000"
-        };
-        const char *namaMetode[] = { "",
+        const char *dataValues[]   = { "", "Angka", "Kata" };
+        const int lengthValues[] = {
+            0, 10000, 50000, 100000, 250000, 
+            500000, 1000000, 1500000, 2000000};
+        const char *methodName[] = { "",
             "Bubble Sort", "Selection Sort", "Insertion Sort",
-            "Merge Sort", "Quick Sort", "Shell Sort"
+            "Merge Sort", "Quick Sort", "Shell Sort", "Semua Metode Sorting"
         };
 
         char choice;
         Confirm:
         clearScreen();
         printf("\nRekap pilihan:\n");
-        printf("  Jenis data   : %s\n", namaData[dataOption]);
-        printf("  Jumlah data  : %s\n", namaLength[lengthOption]);
-        printf("  Metode       : %s\n\n", namaMetode[methodOption]);
+        printf("  Jenis data   : %s\n", dataValues[dataOption]);
+        printf("  Jumlah data  : %d\n", lengthValues[lengthOption]);
+        printf("  Metode       : %s\n\n", methodName[methodOption]);
         printf("Apakah Anda yakin ingin menggunakan sorting tersebut?\n");
         printf("Masukkan Pilihan (Y/N) : ");
         scanf("%c", &choice);
         clearInputBuffer();  // Bersihkan buffer setelah input
         if (choice == 'Y' || choice == 'y') {
             printf("< Memulai Sorting >\n");
+            Sleep(1600);
         } else if (choice == 'N' || choice == 'n') {
             printf("< Membatalkan Sorting >\n");
+            Sleep(1600);
             continue;                   
-        } else if (dataOption   < DATA_ANGKA || dataOption   > DATA_HURUF ||
-                lengthOption < LEN_10K   || lengthOption > LEN_2M      ||
-                methodOption < SORT_BUBBLE || methodOption > MENU_BACK) {
+        } else if ((dataOption   != DATA_ANGKA && dataOption   != DATA_HURUF) ||
+            (lengthOption < LEN_10K     || lengthOption > LEN_2M)       ||
+            (methodOption < SORT_BUBBLE || methodOption > MENU_BACK)) {
             puts("< Terjadi kesalahan: salah satu pilihan menu tidak valid! >");
-            continue;                   
-        } else {
+            Sleep(1600);
+            continue;
+        }
+        else {
             puts("< Input konfirmasi tidak valid, Masukkan input Y atau N >");
             Sleep(1600);
             goto Confirm;
         }
 
         switch (methodOption) {
-            case SORT_BUBBLE:
-                /* panggil bubbleSort(data, size); */
-                break;
-            case SORT_SELECTION:
-                /* panggil selectionSort(...); */
-                break;
-            case SORT_INSERTION:
-                /* panggil insertionSort(...); */
-                break;
-            case SORT_MERGE:
-                /* panggil mergeSort(...); */
-                break;
-            case SORT_QUICK:
-                /* panggil quickSort(...); */
-                break;
-            case SORT_SHELL:
-                /* panggil shellSort(...); */
-                break;
-            case MENU_BACK:
-                /* kembali ke menu sebelumnya */
-                break;
-            default:
-                puts("< Pilihan tidak valid! >");
+        case SORT_BUBBLE:
+            printTableHeader();
+            bubbleSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case SORT_SELECTION:
+            printTableHeader();
+            selectionSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case SORT_INSERTION:
+            printTableHeader();
+            insertionSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case SORT_MERGE:
+            printTableHeader();
+            mergeSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case SORT_QUICK:
+            printTableHeader();
+            quickSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case SORT_SHELL:
+            printTableHeader();
+            shellSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            printTableFooter();
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        case 7: 
+            clearScreen();
+            printf("\n=== Perbandingan Metode Sorting ===\n");
+            
+            // Print header tabel
+            printTableHeader();
+            
+            // Panggil semua fungsi sorting methods
+            bubbleSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            selectionSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            insertionSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            mergeSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            quickSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            shellSort(fileName[dataOption], lengthValues[lengthOption], dataOption);
+            
+            // Print footer tabel
+            printTableFooter();
+
+            puts("\n< Tekan enter untuk lanjut >");
+            getchar();
+            break;
+        default:
+            puts("< Pilihan tidak valid! >");
+        
         }
     }
+
     
     clearScreen();
     return 0;
